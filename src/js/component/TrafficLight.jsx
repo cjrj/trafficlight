b9e48a26a9e6084ca14e80a977fee6d637fd4fc9
cjrj <crivasjayo@gmail.com>
2022-03-02 00:41:45 -0400

    :traffic-light: Entrega de proyecto

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+// import { useState } from "react/cjs/react.production.min";
+
+export function TrafficLight() {
+	const [selectedColor, setSelectedColor] = useState("");
+	return (
+		<div id="container">
+			<div id="post"></div>
+			<div id="box">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor === "red" ? " shine" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" +
+						(selectedColor === "yellow" ? " shine" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" +
+						(selectedColor === "green" ? " shine" : "")
+					}></div>
+			</div>
+		</div>
+	);
+}
